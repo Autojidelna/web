c894e3e47fa88d9dc8a3395e2098bd015fed25e0
mattheroit <verhaegenmatej@gmail.com>
2024-01-22 12:48:31 +0100

    add css to gallery buidler #10

--- .vitepress/theme/components/GalleryBuilder.vue
+++ .vitepress/theme/components/GalleryBuilder.vue
@@ -1,10 +1,41 @@
 <script setup>
-const images = Object.values(import.meta.glob("/public/gallery/*.png", { eager: true, as: "url" }));
-console.log(images);
+const images = Object.values(import.meta.glob("/public/gallery/*.svg", { eager: true, as: "url" }));
+
+function getImageAltText(image) {
+	return image.replace(/^\/public\/gallery\//, "").replace(/\.svg.*$/, "");
+}
 </script>
 
 <template>
-	<div>
-		<img v-for="(image, index) in images" :key="index" :src="image" height="500px" width="500px" alt="Gallery Image" />
+	<div class="info custom-block">
+		<div id="gallery-content">
+			<img v-for="(image, index) in images" :key="index" :src="image" :alt="getImageAltText(image)" />
+		</div>
 	</div>
 </template>
+
+<style>
+#gallery-content {
+	display: flex;
+	flex-wrap: wrap;
+	margin: -10px;
+}
+
+#gallery-content img {
+	width: calc(33.33% - 20px);
+	margin: 10px;
+	box-sizing: border-box;
+}
+
+@media (max-width: 768px) {
+	#gallery-content img {
+		width: calc(50% - 20px);
+	}
+}
+
+@media (max-width: 480px) {
+	#gallery-content img {
+		width: 100%;
+	}
+}
+</style>
