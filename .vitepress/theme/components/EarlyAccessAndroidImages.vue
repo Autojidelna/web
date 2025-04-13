eba87633ae4bf1b6ac22a99b2f015502669f314e
Matěj Verhaegen <matej@appelevate.cz>
2025-04-13 17:48:01 +0200

    replace deprecated

--- .vitepress/theme/components/EarlyAccessAndroidImages.vue
+++ .vitepress/theme/components/EarlyAccessAndroidImages.vue
@@ -1,44 +1,44 @@
 <script setup>
-const images = Object.values(import.meta.glob("/public/early-access/android/*.png", { eager: true, as: "url" }));
+const images = Object.values(import.meta.glob("/public/early-access/android/*.png", { eager: true, query: "?url", import: "default" }));
 
 function getImageAltText(image) {
-	return image.replace(/^\/public\/early-access\/android\//, "").replace(/\.png.*$/, "");
+  return image.replace(/^\/public\/early-access\/android\//, "").replace(/\.png.*$/, "");
 }
 </script>
 
 <template>
-	<div class="info custom-block">
-		<div id="gallery-content">
-			<img v-for="(image, index) in images" :key="index" :src="image" :alt="getImageAltText(image)" />
-		</div>
-	</div>
+  <div class="info custom-block">
+    <div id="gallery-content">
+      <img v-for="(image, index) in images" :key="index" :src="image" :alt="getImageAltText(image)" />
+    </div>
+  </div>
 </template>
 
 <style>
 #gallery-content {
-	display: flex;
-	flex-wrap: wrap;
-	margin: -10px;
+  display: flex;
+  flex-wrap: wrap;
+  margin: -10px;
 }
 
 #gallery-content img {
-	width: calc(50% - 40px);
-	margin: 20px;
-	box-sizing: border-box;
-	border-radius: 8px;
+  width: calc(50% - 40px);
+  margin: 20px;
+  box-sizing: border-box;
+  border-radius: 8px;
 }
 
 @media (max-width: 768px) {
-	#gallery-content img {
-		width: calc(50% - 40px);
-		border-radius: 8px;
-	}
+  #gallery-content img {
+    width: calc(50% - 40px);
+    border-radius: 8px;
+  }
 }
 
 @media (max-width: 480px) {
-	#gallery-content img {
-		width: 100%;
-		border-radius: 8px;
-	}
+  #gallery-content img {
+    width: 100%;
+    border-radius: 8px;
+  }
 }
 </style>
--- .vitepress/theme/components/GalleryBuilder.vue
+++ .vitepress/theme/components/GalleryBuilder.vue
@@ -1,41 +1,41 @@
 <script setup>
-const images = Object.values(import.meta.glob("/public/gallery/*.svg", { eager: true, as: "url" }));
+const images = Object.values(import.meta.glob("/public/gallery/*.svg", { eager: true, query: "?url", import: "default" }));
 
 function getImageAltText(image) {
-	return image.replace(/^\/public\/gallery\//, "").replace(/\.svg.*$/, "");
+  return image.replace(/^\/public\/gallery\//, "").replace(/\.svg.*$/, "");
 }
 </script>
 
 <template>
-	<div class="info custom-block">
-		<div id="gallery-content">
-			<img v-for="(image, index) in images" :key="index" :src="image" :alt="getImageAltText(image)" />
-		</div>
-	</div>
+  <div class="info custom-block">
+    <div id="gallery-content">
+      <img v-for="(image, index) in images" :key="index" :src="image" :alt="getImageAltText(image)" />
+    </div>
+  </div>
 </template>
 
 <style>
 #gallery-content {
-	display: flex;
-	flex-wrap: wrap;
-	margin: -10px;
+  display: flex;
+  flex-wrap: wrap;
+  margin: -10px;
 }
 
 #gallery-content img {
-	width: calc(33.33% - 20px);
-	margin: 10px;
-	box-sizing: border-box;
+  width: calc(33.33% - 20px);
+  margin: 10px;
+  box-sizing: border-box;
 }
 
 @media (max-width: 768px) {
-	#gallery-content img {
-		width: calc(50% - 20px);
-	}
+  #gallery-content img {
+    width: calc(50% - 20px);
+  }
 }
 
 @media (max-width: 480px) {
-	#gallery-content img {
-		width: 100%;
-	}
+  #gallery-content img {
+    width: 100%;
+  }
 }
 </style>
